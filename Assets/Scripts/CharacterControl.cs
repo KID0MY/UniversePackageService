60cfd3236d7bd866abe59983d40206de680b36cf
EGGamer103 <zelemieux@gmail.com>
2025-09-24 16:05:32 -0400

    Basic Movement and Camera Script

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,19 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterControl : MonoBehaviour
 {
-    private 
+    private Rigidbody rb;
+
+    private PlayerInput playerInput;
+
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    [SerializeField] float moveSpeed;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        Cursor.lockState = CursorLockMode.Locked;
+        rb.freezeRotation = true;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = transform.TransformDirection(new Vector3(moveInput.x * moveSpeed, rb.linearVelocity.y, moveInput.y * moveSpeed));
     }
 }
