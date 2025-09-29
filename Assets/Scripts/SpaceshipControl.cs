6537893df7e6e97193693b1fe3693c029a548ab7
EGGamer103 <zelemieux@gmail.com>
2025-09-29 11:36:51 -0400

    warning + thruster SFX and boost function

--- Assets/Scripts/SpaceshipControl.cs
+++ Assets/Scripts/SpaceshipControl.cs
@@ -9,12 +9,21 @@ public class SpaceshipControl : MonoBehaviour
     private Vector2 moveInput;      // Horizontal (x) + Vertical (y)
     private Vector2 mouseInput;     // Mouse X + Mouse Y
     private float rollInput;
-
     private PlayerInput playerInput;
 
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip thrusterClip;
     [SerializeField] private float speedMult = 1;
+    [SerializeField] private float boostMult = 2;
     [SerializeField] private float speedMultAngle = 0.5f;
     [SerializeField] private float speedRollMultAngle = 0.05f;
+    [SerializeField] private float timeLossVal = 2;
+
+    //public Timer time;
+    public float timeLossMult = 1;
+    
+
 
 
     private void Awake()
@@ -27,7 +36,7 @@ public class SpaceshipControl : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         Cursor.lockState = CursorLockMode.Locked;
         rb.angularVelocity = Vector3.zero;
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // ---------------- INPUT CALLBACKS ----------------
@@ -45,10 +54,30 @@ public class SpaceshipControl : MonoBehaviour
     {
         rollInput = context.ReadValue<float>();
     }
+
+    public void OnBoost(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            speedMult = boostMult;
+            timeLossMult = timeLossVal;
+            
+        }
+        else
+        {
+            speedMult = 1;
+            timeLossMult = 1;
+        }
+    }
     // -------------------------------------------------
 
     private void FixedUpdate()
     {
+        if ((moveInput.y < 0 || moveInput.y > 0 || moveInput.x < 0 || moveInput.x > 0) && !audioSource.isPlaying)
+        {
+            audioSource.clip = thrusterClip;
+            audioSource.Play();
+        }
         // Translation
         rb.AddForce(rb.transform.forward * moveInput.y * speedMult, ForceMode.VelocityChange);
         rb.AddForce(rb.transform.right * moveInput.x * speedMult, ForceMode.VelocityChange);
