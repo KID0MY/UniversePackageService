529f4b0276825ca3f7ece5e6c294dfadb559fcab
EGGamer103 <zelemieux@gmail.com>
2025-09-30 12:00:36 -0400

    spawner overlap fixes

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject[] rockPrefabs;
+
+    public int maxRange;
+    public int minRange;
+
+    public int maxCount = 100;
+    int Count;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Count < maxCount)
+        {
+            SpawnPackage();
+            Count++;
+        }
+    }
+
+    public void SpawnPackage()
+    {
+        int spawnPointX = Random.Range(minRange, maxRange);
+        int spawnPointY = Random.Range(minRange, maxRange);
+        int spawnPointZ = Random.Range(minRange, maxRange);
+
+        int rockType = Random.Range(0, 7);
+
+        SphereCollider col = rockPrefabs[rockType].GetComponent<SphereCollider>();
+        Collider[] spawnCheck = Physics.OverlapSphere(col.center, col.radius);
+
+        Vector3 spawnPoint = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
+
+        if (spawnCheck.Length == 0)
+        {
+            Instantiate(rockPrefabs[rockType], spawnPoint, Quaternion.identity);
+        }
+        
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -33,7 +33,25 @@ public class Spawner : MonoBehaviour
 
         Vector3 spawnPoint = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
 
-        if(packageType == 0){Instantiate(packagePrefabs[1], spawnPoint, Quaternion.identity);}
-        else{Instantiate(packagePrefabs[0], spawnPoint, Quaternion.identity);}
+
+
+        if(packageType == 0)
+        {
+            BoxCollider col = packagePrefabs[packageType].GetComponent<BoxCollider>();
+            Collider[] spawnCheck = Physics.OverlapBox(col.center, col.size);
+            if (spawnCheck.Length == 1)
+            {
+                Instantiate(packagePrefabs[0], spawnPoint, Quaternion.identity);
+            }
+        }
+        else
+        {
+            CapsuleCollider col = packagePrefabs[packageType].GetComponent<CapsuleCollider>();
+            Collider[] spawnCheck = Physics.OverlapCapsule(spawnPoint - (Vector3.up * ((col.height/5) - col.radius)), spawnPoint + (Vector3.up * ((col.height / 5) - col.radius)), col.radius);
+            if (spawnCheck.Length == 0)
+            {
+                Instantiate(packagePrefabs[1], spawnPoint, Quaternion.identity);
+            }
+        }
     }
 }
