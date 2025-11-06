915978f0bad36b357508ffa2c7226e09b2cd5922
TitaniumTastesGood <aidenjunta@gmail.com>
2025-11-06 16:29:53 -0500

    Generates some random quests that choose an objective and destination, probably
    Do you have any ibuprofen? I have a headache

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -4,6 +4,7 @@ public class Quest : MonoBehaviour
 {
     public string _questName;
     public string _destination;
+    public string _description;
 
     void Start()
     {
@@ -13,16 +14,45 @@ public class Quest : MonoBehaviour
     public void GenerateRandomQuest()
     {
         GenerateDeliveryType();
-        name = _questName;
+        this.name = _questName;
     }
 
     public void GenerateDeliveryType()
     {
+        GenerateDeliveryDestination();
+        _description = "Yup, this sure is a package.";
         int _questIndex = Random.Range(0, 4);
         switch (_questIndex)
         {
             case 0:
                 _questName = "Basic Delivery";
+                _description = "Bring this package to " + _destination + ".";
+                break;
+            case 1:
+                _questName = "Speedy Delivery";
+                _description = "Bring this package to " + _destination + ", make it speedy, those contents aren't gonna last long.";
+                break;
+            case 2:
+                _questName = "Fragile Delivery";
+                _description = "Bring this package to " + _destination + " carefully. That package is fragile, and I'm not paying you if it breaks.";
+                break;
+            case 3:
+                _questName = "Obscure Delivery";
+                _description = "Bring this package to " + _destination + ", specifically to the core of the planet, cause that's where the recipient lives.";
+                break;
+        }
+    }
+
+    public void GenerateDeliveryDestination()
+    {
+        int _planetIndex = Random.Range(0, 2);
+        switch (_planetIndex)
+        {
+            case 0:
+                _destination = "Planet 1";
+                break;
+            case 1:
+                _destination = "Planet 2";
                 break;
         }
     }
--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,26 +1,39 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
     public static QuestManager _instance;
-    public Quest _questPrefab;
+    public GameObject _questPrefab;
     public List<GameObject> _questList = new List<GameObject>();
 
-    void Awake()
+    void Awake() //Makes this node persist between scenes
     {
         _instance = this;
         DontDestroyOnLoad(gameObject);
-        AddActiveQuest();
     }
 
-    void AddActiveQuest()
+    void Start() //Basic implementation to just shove 4 quests and print them all to console. None of the code in start will be in the final game.
     {
-        Instantiate(_questPrefab);
-        GameObject _newQuest = gameObject.transform.GetChild(gameObject.transform.childCount - 1).gameObject;
-        _questList.Add(_newQuest);
-        _newQuest.GetComponent<Quest>().GenerateRandomQuest();
+        AddActiveQuest();
+        AddActiveQuest();
+        AddActiveQuest();
+        AddActiveQuest();
+        for (int i = 0; i <= 3; i++)
+        {
+            print(_questList[i].GetComponent<Quest>()._questName + " to " + _questList[i].GetComponent<Quest>()._destination + ": " + _questList[i].GetComponent<Quest>()._description);
+        }
+    }
+
+    void AddActiveQuest() //Creates a quest then adds in to _questList :3
+    {
+        GameObject child;
+        child = Instantiate(_questPrefab) as GameObject;
+        child.transform.parent = transform;
+        _questList.Add(child);
+        _questList[_questList.Count - 1].GetComponent<Quest>().GenerateRandomQuest();
     }
 
     void Update()
