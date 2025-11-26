376bd591bde59b5eec9502d53706eea599526427
EGGamer103 <zelemieux@gmail.com>
2025-11-25 22:13:11 -0500

    fixing

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Character movement Values")]
+    public float moveSpeed = 10f;
+    public float gravity = -9.81f;
+
+    // Components
+    private CharacterController controller; 
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    public sceneManager_ sceneManager;
+    
+    private bool isInShipCollisionZone;
+    private bool isInteracting;
+    // Movement
+    private Vector2 moveInput;
+    private Vector3 velocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        velocity.x = 0;
+    }
+
+    // Input System callback
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (isInShipCollisionZone)
+        {
+            sceneManager.loadNextScene();
+        }
+        // You don't need to read the value into isPressing anymore if you use 'context.performed'
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //jump();
+    }
+
+    void jump()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = 5;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        if (controller.isGrounded)
+        {
+            velocity.y = -1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("shipCollision"))
+        {
+            isInShipCollisionZone = true; // Player has entered the zone
+            print("Ship collision");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("shipCollision"))
+        {
+            isInShipCollisionZone = false; // Player has left the zone
+            print("Ship collision out");
+        }
+    }
+}
