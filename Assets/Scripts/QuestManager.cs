056e972698a347e6a9ef3f0d57e058e69ccc9126
EGGamer103 <zelemieux@gmail.com>
2026-03-12 19:29:42 -0400

    shit

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -44,7 +44,7 @@ public class QuestManager : MonoBehaviour
     {
         SetupNameList();
         //autoendtutorial();
-        _enableDebug = true;
+        _enableDebug = false;
     }
 
     public void AddActiveQuest(int planet_exclusion) //Creates a quest then adds in to _questList :3
