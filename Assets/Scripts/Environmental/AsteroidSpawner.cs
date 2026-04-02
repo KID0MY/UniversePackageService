06c4da6d2781729194082a56b95167e3d7439bb2
TitaniumTastesGood <aidenjunta@gmail.com>
2026-04-02 19:42:26 -0400

    fix asteroids

--- Assets/Scripts/Environmental/AsteroidSpawner.cs
+++ Assets/Scripts/Environmental/AsteroidSpawner.cs
@@ -31,16 +31,16 @@ public class AsteroidSpawner : MonoBehaviour
         int rockType = Random.Range(0, rockPrefabs.Length);
 
         SphereCollider col = rockPrefabs[rockType].GetComponent<SphereCollider>();
-        Collider[] spawnCheck = Physics.OverlapSphere(col.center, col.radius);
+        //Collider[] spawnCheck = Physics.OverlapSphere(col.center, col.radius);
 
         Vector3 spawnPoint = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
         GameObject _asteroid = Instantiate(rockPrefabs[rockType], spawnPoint, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
-        _asteroid.transform.localScale = _asteroid.transform.localScale * Random.Range(6, 7);
-        if (spawnCheck.Length == 1)
-        {
-            Debug.Log("pip");
-            Instantiate(rockPrefabs[rockType], spawnPoint, Quaternion.identity);
-        }
+        _asteroid.transform.localScale = _asteroid.transform.localScale * Random.Range(40, 100);
+        //if (spawnCheck.Length == 1)
+        //{
+        //    Debug.Log("pip");
+        //    Instantiate(rockPrefabs[rockType], spawnPoint, Quaternion.identity);
+        //}
         
     }
 }
